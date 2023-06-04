e58ded9c6946d684e0868a1730ffc1b01a261201
Phan Nhat <phandinhnhat1708@gmail.com>
2023-06-04 20:12:45 +0700

    export transitive javafx.graphics

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.vietnam.history {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens com.vietnam.history.controller to javafx.fxml;
     exports com.vietnam.history;
