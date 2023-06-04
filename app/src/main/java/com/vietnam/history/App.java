e8003affd6796450d3db9a5d588cdf3a18670f20
Phan Nhat <phandinhnhat1708@gmail.com>
2023-06-04 20:54:53 +0700

    add icon

--- app/src/main/java/com/vietnam/history/App.java
+++ app/src/main/java/com/vietnam/history/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +20,8 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("StartScene"));
         stage.setScene(scene);
+        Image icon = new Image(getClass().getResourceAsStream("images/icon.png"));
+        stage.getIcons().add(icon);
         stage.show();
     }
 
